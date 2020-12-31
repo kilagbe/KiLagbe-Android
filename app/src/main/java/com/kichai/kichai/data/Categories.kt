55313ea5b6bb9c94da4f091ccd482568e643e0e6
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-12-31 17:37:35 +0600

    Delete the Categories enum class since we don't need it much anymore

--- app/src/main/java/com/kichai/kichai/data/Categories.kt
+++
@@ -1,5 +0,0 @@
-package com.kichai.kichai.data
-
-enum class Categories {
-    Undergraduate, Postgraduate, EnglishMedium, NCTB, HSC, SSC, Abroad, Medical, Engineering, BBA, MBA, Literature, English, Bangla
-}
